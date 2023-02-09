0473e35422d1d985d7d9962893d4d35ef1471171
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2023-02-09 18:49:28 +0530

    create and setup json

--- client/src/components/item-list/ItemList.js
+++ client/src/components/item-list/ItemList.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Item from '../item/Item,'
 import "./style.css"
+import data from "../../data/products.json"
 
 const ItemList = props => {
+
+  const products = data.products;
+
   return (
     <div className='item-list'> 
-    <Item />
-    <Item />
-    <Item />
-    <Item />
-    <Item />
+    {
+      products.map((item, index)=>{
+        return(
+          <Item key={index} name={item.product_name} date={item.created_at}
+           price={item.price} img={item.img_url} city={item.contact_details.city}  />
+        )
+      })
+    }
     </div>
   )
 }
--- client/src/components/item/Item,.js
+++ client/src/components/item/Item,.js
@@ -4,22 +4,25 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import Chip from '@mui/material/Chip'; 
 import { IconButton } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-const Item = (props) => {
+const Item = ({ name, price, img, city, date}) => {
+
+  const d = new Date().toDateString()
+  const val = d.split(" ");
 
   const handleClick = () =>{
-    console.log("lick");
+
+    console.log("hi")
   }
 
   return (
-    <Card className="item" elevation={0} sx={{ textAlign: "right" }}>
+    <Card className="item"  sx={{ textAlign: "right" }}>
     <IconButton sx={{ position: "absolute",ml: -5 }} color="primary" aria-label="add to shopping cart">
     <FavoriteBorderIcon sx={{ fontSize: 25, color: "black" }} />
 </IconButton>
-      <CardMedia className="item-img" component="img" alt="green iguana" sx={{ objectFit: "contain" }} image="https://apollo-singapore.akamaized.net/v1/files/r8i176uj2obj2-IN/image;s=300x600;q=60" />
+      <CardMedia className="item-img" component="img" alt="green iguana" sx={{ objectFit: "contain" }} image={img} />
       <CardContent sx={{ p: 1, "&:last-child": { pb: 0 }, textAlign: "left" }}>
         <Typography
           sx={{
@@ -31,7 +34,7 @@ const Item = (props) => {
           variant="h5"
           component="div"
         >
-          ₹ 39,000 
+          ₹ {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
         </Typography>
         <Typography
         className="item-name"
@@ -43,22 +46,16 @@ const Item = (props) => {
           variant="h1"
           component="div"
         >
-          Apple iPhone 11 Pro
+          {name.slice(0, 20) + "..."}
         </Typography>
-        <Typography
-        className="item-description"
-          sx={{
-            fontFamily: "Montserrat, sans-serif",
-            fontWeight: 600,
-            fontSize: 0.8+"rem"
-          }}
-          variant="body2"
-          color="text.secondary"
-        >
-        Pune | Yesterday
-        </Typography>
-        <div className="item-add-to-cart">
-        <Chip label="Add to Cart" variant="outlined" onClick={handleClick} sx={{ mt: 1 }} />
+        <div className="item-desc">
+          <div className="item-city">
+          {city}
+          </div>
+          <div className="item-date">
+          {val[2]+" "+val[1]}
+          </div>
+
         </div>
       </CardContent>
     </Card>
