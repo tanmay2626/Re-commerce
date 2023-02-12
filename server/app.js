bbf2c46eecb3527652b27722affc804bbc38ce83
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2023-02-12 11:03:42 +0530

    setup server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,45 @@
-console.log("Hello World")
+
+const express = require("express");
+const bodyparser = require("body-parser");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended : true }));
+app.use(cors());
+app.use(bodyparser.json());
+
+mongoose.connect('mongodb://localhost:27017/scrapDB')
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+    mobile_no : {type: String, maxLength: 10},
+    email: String,
+    address : String,
+    city : String
+});
+
+const productSchema = new mongoose.Schema({
+    product_name : String,
+    img_url : String,
+    description : String,
+    price : Number,
+    category : String,
+    brand : String,
+    created_at : Date,
+    usage : String,
+    seller : [userSchema]
+});
+
+const User = mongoose.model("User",userSchema);
+const Product = mongoose.model("Product",productSchema);
+
+app.get("/",(req,res)=>{
+    res.send("Hello Tanmay")
+});
+
+app.listen(8000, ()=>{
+    console.log("Server is running on port 8000");
+});
